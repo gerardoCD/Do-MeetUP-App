d6e988b9dd20fd98f35db1bcffbc71066ed0e4f9
Gerardo <ferrari_gerardo@live.com.mx>
2018-11-26 08:14:50 -0600

    Add new proyect

---
+++ MeetupApp/MeetupApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeetupApp
+//
+//  Created by Gerardo on 11/26/18.
+//  Copyright © 2018 Gerardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
