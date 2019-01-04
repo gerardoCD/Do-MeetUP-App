a4434735dd9700e0bccf3c7a8adbb4ca6ca33a88
Abraham Quezada <abrahamqt@gmail.com>
2019-01-04 15:42:26 -0600

    Cell tickets format

--- MeetupApp/MeetupApp/Controllers/TicketsViewController.swift
+++ MeetupApp/MeetupApp/Controllers/TicketsViewController.swift
@@ -9,36 +9,39 @@
 import UIKit
 
 class TicketsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     var events2 = [Event]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //loadDatas()
+        // Do any additional setup after loading the view.
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "EventTicket", for: indexPath)
+        let cellIdentifier = "TicketTableViewCell"
         
-        cell.textLabel?.text = "Hola"
-     
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? TicketTableViewCell else{
+            fatalError("The dequeued cell is not an instance of TicketTableViewCell.")
+        }
         return cell
     }
-    
-    
-    
-    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loadDatas()
-        // Do any additional setup after loading the view.
-    }
 
     
-    func loadDatas(){
-        Ticket.loadTickets { (events) in
-            self.events2 = events
-        }
-        
-    }
+//    func loadDatas(){
+//        Ticket.loadTickets { (events) in
+//            self.events2 = events
+//        }
+//
+//    }
 
 }
